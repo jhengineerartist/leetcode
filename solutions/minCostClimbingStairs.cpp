003a281af907d3dd634ca00c05acecee262a0a18
= <=>
2023-06-17 21:03:40 -0700

    added minCostClimbingStairs

---
+++ solutions/minCostClimbingStairs.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int min_cost_per_stair[cost.size() + 1];
+        min_cost_per_stair[0] = 0;
+        min_cost_per_stair[1] = 0;
+
+        for (int i = 2; i <= cost.size(); i++)
+        {
+            min_cost_per_stair[i] = min(min_cost_per_stair[i - 1] + cost[i - 1], min_cost_per_stair[i - 2] + cost[i - 2]);
+        }
+
+        return min_cost_per_stair[cost.size()];
+    }
+};
