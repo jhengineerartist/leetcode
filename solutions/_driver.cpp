c1f3ca73d648150133090e196d6ab3041b42d284
Joseph Hirshman <josehirshman@jhengineerartist.com>
2023-06-17 21:03:40 -0700

    added minCostClimbingStairs

--- solutions/_driver.cpp
+++ solutions/_driver.cpp
@@ -1,7 +1,12 @@
+#include "minCostClimbingStairs.cpp"
+#include <vector>
 // Use this as a quick way to run whatever problem
 // I'm working on
 
 int main()
 {
+    Solution sln;
+    vector<int> test = {10, 15, 20};
+    int result = sln.minCostClimbingStairs(test);
     return 0;
 }
